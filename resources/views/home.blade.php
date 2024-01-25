f704dc971ca154e43b8ddc6b97f3941442b6d75c
antoniodalterio <antoniodalterio2227@gmail.com>
2024-01-25 15:27:19 +0100

    feat: Hello World

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body style="background-color: #171923; color: white;">
+    <div style="text-align: center;">
+        <h1 style="font-size: 70px;">Hello World</h1>
+        <h2 style="font-size: 40px">Hello
+            <span style="color: #FF2D20;">Laravel</span>
+        </h2>
+    </div>
+</body>
+
+</html>
